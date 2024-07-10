95b71cb01a9bca965913ac389109b37529b9841a
Husam Abdallah <contact@husamabdallah.com>
2024-07-10 12:10:48 +0300

    #

--- lib/view/screens/address/add_address.dart
+++ lib/view/screens/address/add_address.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shop/core/constants/colors.dart';
 
 class AddAddress extends StatelessWidget {
-  const AddAddress({super.key});
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(15.653120, 32.481529),
+    zoom: 14.4746,
+  );
+  AddAddress({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +21,20 @@ class AddAddress extends StatelessWidget {
       appBar: AppBar(
         title: Text("addAddress".tr),
         foregroundColor: AppColors.white,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: Get.height / 2,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
